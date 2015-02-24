17bf66bae484ca7d62600a64f1529b813a243a1b
Roman Sukhorukov <roman.a.sukhorukov@gmail.com>
2015-02-24 22:46:49 +0300

    Расширение для Firefox. Скелет расширения.

---
+++ add-on/firefox/peepl/src/bootstrap.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const {
+    classes: Cc,
+    interfaces: Ci,
+    results: Cr,
+    utils: Cu
+    } = Components;
+
+Cu.import("resource://gre/modules/Services.jsm");
+
+const APP_NAME = 'peepl';
+
+let core = {
+    get app() {
+        return this._application;
+    },
+
+    init: function () {
+        // FIXME: Переименовать в loadLibraryModules?
+        this._loadModules();
+        this._loadApplication();
+    },
+
+    finalize: function () {
+        this._unloadApplication();
+        this._unloadModules();
+    },
+
+    _loadModules: function () {
+
+    },
+
+    _unloadModules: function () {
+
+    },
+
+    _loadApplication: function () {
+        let scope = {};
+        Cu.import('chrome://' + APP_NAME + '/content/application.jsm', scope);
+
+        this._application = scope.application;
+        if (typeof this._application.init === 'function') {
+            this._application.init(this);
+        }
+    },
+
+    unloadApplication: function () {
+        if (typeof this._application.finalize === 'function') {
+            this._application.finalize();
+        }
+
+        this._application = null;
+    }
+}
+
+function startup (aData, aReason) {
+    core.init();
+}
+
+function shutdown (aData, aReason) {
+    core.finalize();
+}
+
+function install (aData, aReason) {
+
+}
+
+function uninstall (aData, aReason) {
+    if (aReason !== ADDON_UNINSTALL) {
+        return;
+    }
+}
---
+++ add-on/firefox/peepl/src/chrome/content/application.jsm
@@ -0,0 +1,29 @@
+'use strict';
+
+EXPORTED_SYMBOLS = ['application'];
+
+let application = {
+    get core() {
+        return this._core;
+    },
+
+    init: function (aCore) {
+        this._core = aCore;
+
+        this._createUI();
+    },
+
+    finalize: function () {
+        this._destroyUI();
+
+        this._core = null;
+    }
+
+    _createUI: function () {
+
+    },
+
+    _destroyUI: function () {
+
+    }
+}
