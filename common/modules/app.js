3fa9310949ffb7bfd131876b5aa171fdfcc4ada6
Roman Sukhorukov <roman.a.sukhorukov@gmail.com>
2015-03-03 21:54:30 +0300

    Issue #8 Добавил использование navigationMonitor в app.js

--- common/modules/app.js
+++ common/modules/app.js
@@ -1,10 +1,17 @@
+'use strict';
+
 var serverConnector = require('./serverConnector');
+var navigationMonitor = require('./navigation');
 var console = require('./console');
+
+navigationMonitor.init();
+
+navigationMonitor.locationChanged.add(function (aData) {
+    console.log('SIGNAL: navigationMonitor.locationChanged; url=' + aData.url.spec + '; onlyHashChanged=' + aData.onlyHashChanged);
+});
 
 serverConnector.connected.add(function() {
     console.log('connnected from signal!!!');
 });
 
 serverConnector.connect();
-
-
