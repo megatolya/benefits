53caaac51932fee3110d5761a26125a339cf7e8e
Anatoliy Ostrovskiy <forgetthisbox@gmail.com>
2015-03-15 20:09:47 +0300

    cc (userId -> uid)

--- server/controllers/api/whoami.js
+++ server/controllers/api/whoami.js
@@ -3,7 +3,7 @@ var auth = require('../../auth');
 module.exports = function (req, res, next) {
     auth.registerUser().then(function (userInfo) {
         res.json({
-            userId: userInfo.id,
+            uid: userInfo.id,
             salt: userInfo.salt
         });
     }).fail(next);
