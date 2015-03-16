649def3dcabf3ebd9aeaf921a37d6f8fb271ea3c
ilya-b <ilya-b@yandex-team.ru>
2015-03-16 21:54:26 +0300

    Issue #4 add backbone of chrome extension

---
+++ extensions/chrome/modules/navigation.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var signals = require('signals');
+
+module.exports = {
+    locationChanged: new signals.Signal()
+};
--- extensions/modules/app.js
+++ extensions/modules/app.js
@@ -1,35 +1,12 @@
 'use strict';
 
 var serverConnector = require('./serverConnector');
-var navigationMonitor = require('../impl/navigation');
-var downloadsMonitor = require('../impl/downloads');
 var console = require('../impl/console');
 
 var app = {
     start: function () {
         console.log('starting application...');
-
-        // TODO: Убрать эту функцию, модули будут импортироваться там, где они нужны,
-        // клиенты сами будут ожидать инициализации через callback.
-        this._initModules();
-    },
-
-    // TODO: firefox Как-то научиться вызывать finalize из bootstrap.js
-    finalize: function () {
-
-    },
-
-    _initModules: function () {
-        // TODO удалить зависимости от initializer
-        // initializer.initModules([downloadsMonitor], function (errorStatus) {
-        //    if (errorStatus) {
-        //        console.log('DownloadsMonitor initialization failed. ' + errorStatus);
-        //    } else {
-        //        console.log('DownloadsMonitor initialization succeed.');
-        //    }
-        // });
-        //
-        // navigationMonitor.start();
+        serverConnector.connect();
     }
 };
 
