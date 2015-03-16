871f06c703db765b67317f04b6c38cd9ef080d6f
ilya-b <ilya-b@yandex-team.ru>
2015-03-16 11:23:53 +0300

    Issue #14 add copy:out task

---
+++ extensions/build/copy.js
@@ -0,0 +1,27 @@
+'use strict';
+
+
+module.exports = function (grunt) {
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+    grunt.config('copy.impl', {
+        expand: true,
+        cwd: '<%= browser_modules_path %>/',
+        src: '**',
+        dest: '<%= impl_path %>/'
+    });
+
+    grunt.config('copy.out', {
+        expand: true,
+        cwd: '<%= browser_src_path %>/',
+        filter: function (path) {
+            return !/.*\/modules\/.*/.test(path);
+        },
+        src: ['**'],
+        dest: '<%= dist_path %>/'
+    });
+
+    grunt.registerTask('copy-impl', function () {
+        grunt.task.run(['copy:impl', 'copy:out']);
+    });
+};
