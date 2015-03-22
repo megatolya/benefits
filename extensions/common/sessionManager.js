1faa04144cc51db76d895aa15567bf4b8d761cfa
ilya-b <ilya-b@yandex-team.ru>
2015-03-22 15:18:42 +0300

    Issue #28 Добавил модуль sessionData, вынес startSession из app.js в sessionManager

--- extensions/common/sessionManager.js
+++ extensions/common/sessionManager.js
@@ -1,66 +1,30 @@
 'use strict';
 
-var storage = require('common/storage');
+var serverConnector = require('common/serverConnector');
+var sessionData = require('common/sessionData');
 var console = require('specific/console');
 
-var sessionManagerWrapper = (function () {
-    var storageKeys = {
-        UID: 'extesion-uid',
-        SALT: 'salt'
-    };
+function handleWhoamiError(error) {
+    // FIXME: Использовать логгер
+    console.log('Start session failed. ' + error);
+    return Promise.reject(error);
+}
 
-    function md5(aString) {
-        return aString;
-    }
+function handleWhoamiResponse(response) {
+    var data = response.whoami;
+    console.log('Whoami response: ' + data);
+    sessionData.setUID(data.uid);
+    sessionData.setSalt(data.salt);
+}
 
-    return {
-        /**
-         * Возвращает токен.
-         *
-         * @param {String} aMethod - имя метода, для которого нужно получить токен.
-         *
-         * @returns {String}
-         */
-        getToken: function (aMethod) {
-            if (aMethod === 'whoami') {
-                return '9990da9c91b76eaacd9addfd3dbba36f';
-            }
-
-            var uid = this.getUID();
-            var salt = this.getSalt();
-
-            // FIXME: Заменить md5 на реализацию
-            return md5(uid + salt + aMethod);
-        },
-
-        /**
-         * Возвращает id сессии.
-         *
-         * @returns {String}
-         */
-        getUID: function () {
-            return storage.get(storageKeys.UID) || '';
-        },
-
-        setUID: function (aUID) {
-            storage.set(storageKeys.UID, aUID);
-        },
-
-        /**
-         * Устанавливает текущую соль.
-         *
-         * @param {String} aSaltString
-         */
-        setSalt: function (aSaltString) {
-            if (aSaltString) {
-                storage.set(storageKeys.SALT, aSaltString);
-            }
-        },
-
-        getSalt: function () {
-            return storage.get(storageKeys.SALT) || '';
+module.exports = {
+    startSession: function () {
+        if (sessionData.getSalt() && sessionData.getUID()) {
+            console.log('Session already exist');
+            return Promise.resolve();
         }
-    };
-})();
-
-module.exports = sessionManagerWrapper;
+        return serverConnector.whoami()
+            .then(handleWhoamiResponse)
+            .catch(handleWhoamiError);
+    }
+};
