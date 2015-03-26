18e12aa96ffdf52bfbffe8fe6e72274730d80033
Anatoliy Ostrovskiy <forgetthisbox@gmail.com>
2015-03-27 00:31:00 +0300

    cc Перенес checkToken в middleware

--- server/auth/middleware.js
+++ server/auth/middleware.js
@@ -3,9 +3,41 @@
 var db = require('../db');
 var config = require('../config');
 var console = require('../console');
+var auth = require('./');
+var Q = require('q');
 
 function log(req, status, params) {
     console.log(req.method + ' ' + req.path + ' (' + status + ')', params || '');
+}
+
+function checkToken(req, token, uid) {
+    if (config.useToken === false) {
+        return Q.resolve();
+    }
+
+    var deferred = Q.defer();
+
+    var method = req.path.replace('/api/v1/', '');
+    // находим пользователя, его соль, генерим токен и сравниваем
+    db.users.get(uid)
+        .then(function (user) {
+            if (!user) {
+                deferred.reject(new Error('User not found'));
+                return;
+            }
+
+            var validToken = auth.generateToken(uid, user.salt, method);
+            console.log('validToken', validToken, 'vs token', token);
+
+            if (token === validToken) {
+                deferred.resolve();
+            } else {
+                deferred.reject(new Error('Wrong token'));
+            }
+        })
+        .fail(deferred.reject);
+
+    return deferred.promise;
 }
 
 module.exports = function (req, res, next) {
@@ -19,7 +51,7 @@ module.exports = function (req, res, next) {
         next();
         return;
     } else {
-        req.checkToken(params.token, params.uid).then(function () {
+        checkToken(req, params.token, params.uid).then(function () {
             req.authorized = true;
             log(req, 'authorized');
             next();
