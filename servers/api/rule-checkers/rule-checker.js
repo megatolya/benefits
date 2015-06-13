1782f4d255056bd76978124aa75f9be8b314609f
ilya-b <ilya-b@yandex-team.ru>
2015-06-13 13:30:17 +0300

    Simplify rule-checkers

---
+++ servers/api/rule-checkers/rule-checker.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var debug = require('debug')('app:rule-checker');
+
+function isFunction(fn) {
+    return typeof fn === 'function';
+}
+
+module.exports = {
+    check: function (data, rule) {
+        if (!data || !rule) {
+            return false;
+        }
+        var method = this._methods[rule.type];
+        if (isFunction(method)) {
+            return method.call(this, data, rule);
+        }
+        debug('method do not exist: ', rule.type);
+        return false;
+    },
+
+    _methods: {
+        compareLess: function (data, rule) {
+            return data[rule.field] < rule.aim;
+        },
+
+        compareMore: function (data, rule) {
+            return data[rule.field] > rule.aim;
+        }
+    }
+};
